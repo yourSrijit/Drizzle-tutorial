d07693db2cd7437cfdb421b94d187fdccec83a39
yourSrijit <berasrijit02@gmail.com>
2024-09-10 18:57:59 +0530

    find using where and contitional operator

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express=require("express");
 const db = require("./db.config");
 const { TodoTable } = require("./schema");
+const { eq } = require("drizzle-orm");
 const app=express();
 
 require('dotenv').config();
@@ -10,11 +11,18 @@ const port=process.env.PORT || 3000
 
 app.get("/",async(req,res)=>{
 
-    const item=({
-        title:"Moring walk",
-        desc:"Today i will definitly go for moring walk.This is my duety"
-    })
-    const todos=await db.insert(TodoTable).values(item).returning()
+    // 1. Insertion DOne 
+    // const item=({
+    //     title:"Moring walk",
+    //     desc:"Today i will definitly go for moring walk.This is my duety"
+    // })
+    // const todos=await db.insert(TodoTable).values(item).returning()
+
+    // 2. Get data 
+    const todos=await db.select({
+        title:TodoTable.title,
+        desc:TodoTable.desc
+    }).from(TodoTable).where(eq(TodoTable.id,1))
     return res.send({
         message:
         todos
